a240cdc72140a9da2053de805db58cbc574c7cf0
3liqm <MrAliMalik10@outlook.com>
2023-12-25 15:30:58 +0300

    first commit

---
+++ src/constants/images.js
@@ -0,0 +1,54 @@
+import email from '../assets/email.png';
+import mobile from '../assets/mobile.png';
+import api from '../assets/api.png';
+import css from '../assets/css.png';
+import git from '../assets/git.png';
+import html from '../assets/html.png';
+import javascript from '../assets/javascript.png';
+import python from '../assets/python.png';
+import react from '../assets/react.png';
+import redux from '../assets/redux.png';
+import sass from '../assets/sass.png';
+import typescript from '../assets/typescript.png';
+import next from '../assets/next.png';
+
+import about01 from '../assets/about01.png';
+import about02 from '../assets/about02.png';
+import about03 from '../assets/about03.png';
+import about04 from '../assets/about04.png';
+
+import profile from '../assets/profile.png';
+import circle from '../assets/circle.svg';
+
+import adidas from '../assets/adidas.png';
+import amazon from '../assets/amazon.png';
+
+import nb from '../assets/nb.png';
+
+
+export default {
+  email,
+  mobile,
+  api,
+  css,
+  git,
+  html,
+  javascript,
+  python,
+  react,
+  redux,
+  sass,
+  typescript,
+  next,
+  about01,
+  about02,
+  about03,
+  about04,
+  profile,
+  circle,
+  adidas,
+  amazon,
+  nb,
+
+  
+};
