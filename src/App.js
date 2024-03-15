d2b3de41ffeecdc25375efa36c99459ff01c9317
Ali Malik <146491358+3liqm@users.noreply.github.com>
2024-03-15 17:38:23 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
 import { Navbar } from "./components";
-import "./app.scss";
+import "./App.scss";
 
 const App = () => {
   //Dark mode 
